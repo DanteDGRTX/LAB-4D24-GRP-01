8a79951e8c85a2b89fa9c5d683d43f1b91aafe04
Ronal Dante Gonzales Quispe <ronal.gonzales@tecsup.edu.pe>
2026-08-29 13:20:59 -0500

    Update StudentModel in sprint-1

--- src/StudentModel.java
+++ src/StudentModel.java
@@ -1,12 +1,18 @@
 public class StudentModel {
 
     private String name;
+    private String email;
 
-    public StudentModel(String name) {
+    public StudentModel(String name, String email) {
         this.name = name;
+        this.email = email;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
